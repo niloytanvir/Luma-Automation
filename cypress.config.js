2b95d08dd2f4e638a6c1f0dcf65e07bae5fe6e63
TanvirNiloy <niloytanvirahmed@gmail.com>
2025-07-13 13:03:18 +0600

    adding parallel test on different browser and generating html reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
+    e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+  reporter: 'mochawesome',
+  reporterOptions: {
+   //reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+})
